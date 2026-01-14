4facef269a0c6c5265ae4a652d439780b713366a
nicolastaf <nicolas.staffent@gmail.com>
2026-01-14 17:19:12 +0100

    EXO P1C5 fait

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,13 @@
 # Écrivez votre code ici !
+nom = "Alice"
+age = 30
+taille = 1.75
+est_etudiant = True
+print(f"Nom:{nom}")
+print(f"Age:{age}")
+print(f"Taille:{taille}")
+print(f"Est étudiant:{est_etudiant}")
+print(f"Type nom: {type(nom)}")
+print(f"Type age: {type(age)}")
+print(f"Type taille: {type(taille)}")
+print(f"Type est étudiant: {type(est_etudiant)}")
