f08fbc352fea00e62e5c59999815e3b43098b65b
nicolastaf <nicolas.staffent@gmail.com>
2026-01-14 18:06:15 +0100

    EXO P1C7 fait

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,9 @@
 # Écrivez votre code ici !
+fruits = {"pomme": "Rouge", "banane": "Jaune", "orange": "Orange"}
+# Ajout de la clé "Kiwi"
+fruits["Kiwi"] = "Vert"
+
+couleur_banane = fruits["banane"]
+
+del fruits["banane"]
+print(fruits.keys())
