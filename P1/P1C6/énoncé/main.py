7f00d0cef8b5e7217ef866a6b0778ae4043a459f
nicolastaf <nicolas.staffent@gmail.com>
2026-01-14 17:38:05 +0100

    EXO P1C6 fait

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,8 @@
 # Écrivez votre code ici !
+fruits = ["pomme","banane","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+# 4. Modification du deuxième élément en "ananas"
+fruits[1] = "ananas"
+fruits.sort()
+print(f"La liste des fruits contient {len(fruits)} éléments.")
